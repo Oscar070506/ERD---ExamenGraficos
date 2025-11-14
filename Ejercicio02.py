7b60c99bf06302bb852ef67419adbb6f4e61a43c
Oscar070506 <2006ospi@gmail.com>
2025-11-14 20:40:14 +0100

    Update Ejercicio02.py

--- Ejercicio02.py
+++ Ejercicio02.py
@@ -8,7 +8,6 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Gráfico de barras mejorado
 plt.figure(figsize=(8,5))
 bars = plt.bar(df['productes'], df['vendes'], color=["#A50101","#00458A","#00A000","#F59C44","#720081"])
 plt.title("Productos y Ventas", fontsize=16, fontweight='bold')
@@ -19,5 +18,6 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 for bar in bars:
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2, yval + 20, f'{yval}', ha='center', fontsize=10)
+
 
 plt.show()
