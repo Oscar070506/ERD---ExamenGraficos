4d7c2a77d4cd3304ef06550b04ca0c94791e5455
Oscar070506 <2006ospi@gmail.com>
2025-11-14 20:41:43 +0100

    Reformat Ejercicio05.py for consistency

--- Ejercicio05.py
+++ Ejercicio05.py
@@ -2,11 +2,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Simulación de datos
 notes = np.random.randint(0, 11, 30)
 df = {'x': notes}
 
-# Histograma
 sns.histplot(x = notes, kde = True,
              line_kws = {'linestyle':'dashed',
                          'linewidth':'2'}).lines[0].set_color('red') 
@@ -15,4 +13,5 @@ plt.title('Distribució de les notes de l\'exàmen', fontsize=16)
 plt.xlabel('Nota', fontsize=14)
 plt.ylabel('Cuantitat', fontsize=14)
 plt.grid(True, linestyle='--', alpha=0.5)
+
 plt.show()
